915213916153b4d6185bd784e910d61b96615ff2
pinnarbolu-ctrl <pinnarbolu@gmail.com>
2026-06-03 10:10:13 +0300

    Update btcturk_sinyal.py
    Token eklendi

--- btcturk_sinyal.py
+++ btcturk_sinyal.py
@@ -4,7 +4,7 @@ import time
 import feedparser
 from deep_translator import GoogleTranslator
 
-BOT_TOKEN = "BURAYA_TOKEN_YAZ"
+BOT_TOKEN = "8553499613:AAHkPbN5W4MS3NlwtN0HRH7PyFt_aXLiChQ"
 
 CHAT_IDS = [
     2097448038,
