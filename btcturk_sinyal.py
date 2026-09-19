eac717c85a3375e72f70c096dfcfdee455c0825d
pinnarbolu-ctrl <pinnarbolu@gmail.com>
2026-09-19 17:59:47 +0300

    Update btcturk_sinyal.py

--- btcturk_sinyal.py
+++ btcturk_sinyal.py
@@ -25,7 +25,7 @@ import uuid
 from decimal import Decimal, ROUND_DOWN
 
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "").strip()
 
 CHAT_IDS = [2097448038]
 
@@ -1669,7 +1669,7 @@ NEGATIF = [
 
 def telegram_gonder(mesaj):
     if not BOT_TOKEN:
-        print("BOT_TOKEN bulunamadı. Railway Variables kontrol et.")
+        print("TELEGRAM_BOT_TOKEN bulunamadı. Railway Variables kontrol et.")
         return
 
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
